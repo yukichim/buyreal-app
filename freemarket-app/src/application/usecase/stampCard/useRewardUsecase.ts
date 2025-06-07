e89ae3b38910a9f094de93585075c7f1b92ca5cf
yukimaeda <maedayuuki@maedayuukinoMacBook-Air.local>
2025-06-07 15:53:33 +0900

    imple: usecase use reward

---
+++ freemarket-app/src/application/usecase/stampCard/useRewardUsecase.ts
@@ -0,0 +1,32 @@
+import type { StampCardRepository } from "~/domain/repositories/stampCardRepository";
+
+export class UseRewardUseCaseInputData {
+	constructor(private userId: string) {}
+
+	get UserId(): string {
+		return this.userId;
+	}
+}
+
+interface UseRewardUseCase {
+	execute(input: UseRewardUseCaseInputData): Promise<void>;
+}
+
+export class UseRewardUseCaseInteractor implements UseRewardUseCase {
+	constructor(private stampCardRepository: StampCardRepository) {}
+
+	async execute(input: UseRewardUseCaseInputData): Promise<void> {
+		const stampCard = await this.stampCardRepository.findByUserId(input.UserId);
+
+		if (!stampCard) {
+			throw new Error("スタンプカードが見つかりません");
+		}
+
+		if (!stampCard.canGetReward()) {
+			throw new Error("スタンプが足りません");
+		}
+
+		stampCard.useReward();
+		await this.stampCardRepository.save(stampCard);
+	}
+}
