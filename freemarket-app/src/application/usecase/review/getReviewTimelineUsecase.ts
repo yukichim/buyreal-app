86c5f7336b9d64aa21358070fd39e18b52c93110
yukimaeda <maedayuuki@maedayuukinoMacBook-Air.local>
2025-06-07 15:43:14 +0900

    imple: usecase get timeline

---
+++ freemarket-app/src/application/usecase/review/getReviewTimelineUsecase.ts
@@ -0,0 +1,31 @@
+import type { ReviewRepository } from "~/domain/repositories/reviewRepository";
+import type { ReviewEntity } from "~/domain/entities/review";
+
+export class GetReviewTimelineUseCaseInputData {
+	constructor(private limit: number) {
+		if (limit < 0) {
+			throw new Error("limit must over 1.");
+		}
+	}
+
+	get Limit(): number {
+		return this.limit;
+	}
+}
+
+interface GetReviewTimelineUseCase {
+	execute(request: GetReviewTimelineUseCaseInputData): Promise<ReviewEntity[]>;
+}
+
+// TODO: Response(Outputdata)は別途実装
+export class GetReviewTimelineUseCaseInteractor
+	implements GetReviewTimelineUseCase
+{
+	constructor(private reviewRepository: ReviewRepository) {}
+
+	async execute(
+		request: GetReviewTimelineUseCaseInputData,
+	): Promise<ReviewEntity[]> {
+		return await this.reviewRepository.findRecent(request.Limit);
+	}
+}
