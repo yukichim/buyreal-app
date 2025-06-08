0e7946090ea6421097cdfa1c5495fea182c4c90e
yuki.maeda <maedayuuki@maedayuukinoMacBook-Air.local>
2025-06-08 15:36:47 +0900

    fix: trpc provider

--- freemarket-app/src/features/providers/trpcProvider.tsx
+++ freemarket-app/src/features/providers/trpcProvider.tsx
@@ -14,6 +14,7 @@ export function TrpcProvider({ children }: { children: React.ReactNode }) {
 			links: [
 				httpBatchLink({
 					url: "/api/trpc",
+					transformer: undefined,
 				}),
 			],
 		}),
