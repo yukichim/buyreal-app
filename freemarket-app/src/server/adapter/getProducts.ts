cdcf8f8c76c42c16cd58d2ada6b2cc54583313e8
yuki.maeda <maedayuuki@maedayuukinoMacBook-Air.local>
2025-06-08 21:34:57 +0900

    imple: adapter getProduct

---
+++ freemarket-app/src/server/adapter/getProducts.ts
@@ -0,0 +1,59 @@
+import {
+	GetProductUseCaseInputData,
+	type GetProductUseCase,
+} from "~/application/usecase/product/getProductUsecase";
+import type {
+	Money,
+	ProductCondition,
+	ProductEntity,
+	ProductStatus,
+} from "~/domain/entities/product";
+
+export type ResponseGetByPage = {
+	id: string;
+	title: string;
+	description: string;
+	price: Money;
+	condition: ProductCondition;
+	status: ProductStatus;
+	sellerId: string;
+	categoryId: string;
+	images: string[];
+	createdAt: Date;
+	updatedAt: Date;
+};
+
+export class GetProductAdapter {
+	constructor(private usecase: GetProductUseCase) {}
+
+	async getByPage(): Promise<ResponseGetByPage[] | null> {
+		const usecaseInput = new GetProductUseCaseInputData({});
+		const useCaseOutput = await this.usecase.execute(usecaseInput);
+		const adapterResponse = this.convertToResponse(useCaseOutput);
+		return adapterResponse;
+	}
+
+	private convertToResponse(
+		output: ProductEntity[] | null,
+	): ResponseGetByPage[] | null {
+		if (!output) {
+			console.log("get products return data is null.");
+			return null;
+		}
+		return output?.map((e) => {
+			return {
+				id: e.Id.value,
+				categoryId: e.CategoryId,
+				condition: e.Condition,
+				description: e.Descripyion,
+				images: e.Images,
+				price: e.Price,
+				sellerId: e.SellerId,
+				status: e.Status,
+				title: e.Title,
+				updatedAt: e.UpdatedAt,
+				createdAt: e.CreatedAt,
+			} as ResponseGetByPage;
+		});
+	}
+}
